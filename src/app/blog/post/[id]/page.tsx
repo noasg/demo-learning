444636839b7d6b8843538c3f899dcb0fa6b6f263
adrian.popescu <adrian.popescu@gmail.com>
2025-08-10 19:08:33 +0300

    test push

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,15 @@
-import { posts } from '@/app/lib/placeholder-data';
-import Post from '@/app/ui/components/posts/Post';
+import { posts } from "@/app/lib/placeholder-data";
+import Post from "@/app/ui/components/posts/Post";
 
 export default function Page({ params }: { params: { id: string } }) {
   const post = posts.find((post) => post.id === params.id);
+  if (!post) {
+    return <h1>Post not found</h1>;
+  }
   return (
     <>
       <h1>Post</h1>
       <Post {...post} />
-    </>)
+    </>
+  );
 }
